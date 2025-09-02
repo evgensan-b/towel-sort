a7f1a073739807566b17a1902194690c5c5a8289
EvgenSan <evgnesan@yandex.ru>
2025-09-02 14:59:34 +0300

    feat: solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  const resultArr = matrix.map((element, index) =>
+    index % 2 !== 0 ? element.slice().reverse() : element
+  );
+  const result = resultArr.flat();
+
+  return result;
 };
